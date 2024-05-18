f1b07c6d3a7fe915fb3220e316d14a56dec114ab
Arthur <maiaarthur744@gmail.com>
2024-05-18 19:31:54 -0300

    trying to read file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import struct
 from root_entry import RootEntry
 
 iso_directory = "./iso"
@@ -26,6 +27,27 @@ def get_root_size(filePath, rootOffset):
         # print("Size of root directory:", root_size, "bytes")
         return root_size
 
+# def get_next_cluster(img, boot_params, cluster):
+#     fat_offset = boot_params['reserved_sectors'] * boot_params['bytes_per_sector'] + cluster * 2
+#     img.seek(fat_offset)
+#     return struct.unpack('<H', img.read(2))[0]
+# 
+# def read_file_content(file_path, boot_params, root_offset, cluster, size):
+#     sectors_per_cluster = boot_params['sectors_per_cluster']
+#     bytes_per_sector = boot_params['bytes_per_sector']
+#     root_dir_sector, _ = root_offset
+#     data_region_start = root_dir_sector + (boot_params['max_root_dir_entries'] * 32) // bytes_per_sector
+# 
+#     content = bytearray()
+#     while size > 0:
+#         sector = data_region_start + (cluster - 2) * sectors_per_cluster
+#         file_path.seek(sector * bytes_per_sector)
+#         cluster_data = file_path.read(min(size, sectors_per_cluster * bytes_per_sector))
+#         content.extend(cluster_data)
+#         size -= len(cluster_data)
+#         cluster = get_next_cluster(file_path, boot_params, cluster)
+# 
+#     return content.decode('ascii', errors='replace')
 
 file_path = get_file(iso_directory)
 if file_path:
@@ -57,6 +79,7 @@ if file_path:
 
         entry_number = root_size // 32
         files_in_root = []
+        root_offset_original = root_offset
 
         for entry in range(entry_number):
             f.seek(root_offset)
@@ -70,6 +93,9 @@ if file_path:
 
             size = int.from_bytes(dir_entry[28:32], byteorder='little')
 
+            starting_cluster = int.from_bytes(dir_entry[26:28], byteorder='little')
+            # content = read_file_content(file_path, boot_params, root_offset_original, starting_cluster, size) TODO ver se isso funciona
+            
             date_bytes = dir_entry[16:18]
             date_value = int.from_bytes(date_bytes, byteorder='little')
             day = date_value & 0x1F
@@ -81,7 +107,7 @@ if file_path:
             files_in_root.append(RootEntry(
                 size = size,  # OK
                 full_name = full_filename,  # OK
-                content = b"",  #
+                content = content,  # NOT OK
                 entry_type = file_type,  # OK
                 last_updated = f"{year}-{month:02d}-{day:02d}"  # OK
             ))
